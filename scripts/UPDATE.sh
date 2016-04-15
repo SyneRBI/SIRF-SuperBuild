6c590a01bf89853067b98d4e51a089b9dd52fe71
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2016-04-15 17:29:24 +0100

    added update script

---
+++ scripts/UPDATE.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#cd ~/devel/gadgetron
+#git pull
+#cd build
+#make
+#make install
+
+#cd ~/devel/STIR
+#git pull
+#cd ../build/STIR
+#make
+#make install
+
+cd ~/devel/xSTIR
+git pull
+cd pSTIR
+make
+
+cd ~/devel/iUtilities
+git pull 
+make clean 
+make
+
+cd ../xGadgetron
+git pull
+cd cGadgetron
+make clean 
+make
+cd ../pGadgetron
+make clean
+make
+cd ../examples
+make
+
+
