9a9473e3e61405037c0bd568c11378f160e8d40e
Evgueni Ovtchinnikov <eeo@CCPPETMR>
2016-08-12 13:31:37 +0100

    corrected ADD_REMOTE.sh

--- scripts/ADD_REMOTE.sh
+++ scripts/ADD_REMOTE.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+cd ~/devel/ismrmrd
 git remote add upstream https://github.com/ismrmrd/ismrmrd.git
+
+cd ~/devel/gadgetron
 git remote add upstream https://github.com/gadgetron/gadgetron.git
+
+cd ~/devel/STIR
 git remote add upstream https://github.com/UCL/STIR.git
