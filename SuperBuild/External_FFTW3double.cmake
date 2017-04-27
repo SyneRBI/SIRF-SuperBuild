bedaa69de26ce989b451d2f1e3c67225c731f905
Edoardo Pasca <edo.paskino@gmail.com>
2017-04-27 09:20:00 +0100

    added FFTW3double as dependency for Gadgetron

---
+++ SuperBuild/External_FFTW3double.cmake
@@ -0,0 +1,58 @@
+#These need to be unique globally
+set(externalProjName FFTW3double)
+set(proj FFTW3double)
+set(proj_COMPONENTS "COMPONENTS single")
+# Set dependency list
+set(${proj}_DEPENDENCIES "")
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(NOT ( DEFINED "USE_SYSTEM_${externalProjName}" AND "${USE_SYSTEM_${externalProjName}}" ) )
+  message(STATUS "${__indent}Adding project ${proj}")
+
+  ### --- Project specific additions here
+  set(FFTWdouble_Install_Dir ${CMAKE_CURRENT_BINARY_DIR}/INSTALL)
+  set(FFTWdouble_Configure_Script ${CMAKE_CURRENT_LIST_DIR}/External_FFTWdouble_configure.cmake)
+  set(FFTW_Build_Script ${CMAKE_CURRENT_LIST_DIR}/External_FFTWdouble_build.cmake)
+
+  set(${proj}_URL http://www.fftw.org/fftw-3.3.5.tar.gz )
+  set(${proj}_MD5 6cc08a3b9c7ee06fdd5b9eb02e06f569 )
+
+  if(CMAKE_COMPILER_IS_CLANGXX)
+    set(CLANG_ARG -DCMAKE_COMPILER_IS_CLANGXX:BOOL=ON)
+  endif()
+
+  set(FFTWdouble_SOURCE_DIR ${SOURCE_DOWNLOAD_CACHE}/${proj} )
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    URL ${${proj}_URL}
+    URL_HASH MD5=${${proj}_MD5}
+    SOURCE_DIR ${FFTWdouble_SOURCE_DIR}
+    BINARY_DIR ${FFTWdouble_SOURCE_DIR}
+    CONFIGURE_COMMAND ./configure --with-pic --prefix ${FFTWdouble_Install_Dir}
+    INSTALL_DIR ${FFTWdouble_Install_Dir}
+  )
+
+  #set(FFTW_ROOT        ${FFTW_SOURCE_DIR})
+  #set(FFTW_INCLUDE_DIR ${FFTW_SOURCE_DIR})
+
+  set( FFTW3double_DIR ${FFTWdouble_Install_Dir} )
+  set( FFTW3double_ROOT ${FFTWdouble_Install_Dir} )
+
+
+ else()
+    if(${USE_SYSTEM_${externalProjName}})
+      find_package(${proj} ${${externalProjName}_REQUIRED_VERSION} ${${externalProjName}_COMPONENTS} REQUIRED)
+      message(STATUS "USING the system ${externalProjName}, found FFTW3double_INCLUDE_DIR=${FFTW3double_INCLUDE_DIR}, FFTW3double_LIBRARY=${FFTW3double_LIBRARY}")
+  endif()
+  ExternalProject_Add_Empty(${proj} "${${proj}_DEPENDENCIES}")
+endif()
+
+mark_as_superbuild(
+  VARS
+    ${externalProjName}_DIR:PATH
+  LABELS
+    "FIND_PACKAGE"
+)
--- SuperBuild/External_Gadgetron.cmake
+++ SuperBuild/External_Gadgetron.cmake
@@ -3,7 +3,7 @@ set(externalProjName Gadgetron)
 set(proj Gadgetron)
 
 # Set dependency list
-set(${proj}_DEPENDENCIES "Boost;HDF5;ISMRMRD;FFTW3;googletest;armadillo")
+set(${proj}_DEPENDENCIES "Boost;HDF5;ISMRMRD;FFTW3double;googletest;armadillo")
 
 message(STATUS "MATLAB_ROOT=" ${MATLAB_ROOT})
 #message(STATUS "STIR_DIR=" ${STIR_DIR})
@@ -38,8 +38,10 @@ if(NOT ( DEFINED "USE_SYSTEM_${externalProjName}" AND "${USE_SYSTEM_${externalPr
         -DHDF5_ROOT=${HDF5_ROOT}
         -DHDF5_INCLUDE_DIRS=${HDF5_INCLUDE_DIRS}
         -DHDF5_LIBRARIES=${HDF5_LIBRARIES}
+        -DFFTW_LIBRARIES=${CMAKE_CURRENT_BINARY_DIR}/INSTALL/lib
+         -DFFTW_INCLUDES=${CMAKE_CURRENT_BINARY_DIR}/INSTALL/include
         -DISMRMRD_DIR=${ISMRMRD_DIR}
-    INSTALL_DIR ${Gadgetron_Install_Dir}
+	    INSTALL_DIR ${Gadgetron_Install_Dir}
     DEPENDS
         ${${proj}_DEPENDENCIES}
   )
