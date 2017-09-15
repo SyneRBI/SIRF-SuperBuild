229ea4ffda33db018ff811b264e1f80cfb2e4ad6
Edoardo Pasca <edo.paskino@gmail.com>
2017-09-15 14:39:44 +0000

    Allow user to set URL/TAG for STIR, Gadgetron, SIRF, ISMRMRD
    The user can now override the default configuration by setting the values of
    <package>_TAG <package>_URL for each of STIR, SIRF, Gadgetron and ISMRMRD packages
    
    Corrects a bug associating default URL to TAG.

--- version_config.cmake
+++ version_config.cmake
@@ -69,37 +69,56 @@ set(googletest_URL https://github.com/google/googletest )
 set(googletest_TAG release-1.8.0)
 
 option (DEVEL_BUILD "Developer Build" OFF)
+
+#Set the default versions for SIRF, STIR, Gadgetron and ISMRMRD
+# with devel build it uses latest version of upstream packages
+# otherwise uses the versions for SIRF 0.9
+
 #SIRF
 set(SIRF_URL https://github.com/CCPPETMR/SIRF )
 if (DEVEL_BUILD)
   set (SIRF_TAG master)
   ## STIR
-  set(STIR_URL https://github.com/UCL/STIR )
-  set(STIR_TAG master)
+  set(DEFAULT_STIR_URL https://github.com/UCL/STIR )
+  set(DEFAULT_STIR_TAG master)
 
   ## Gadgetron
-  set(Gadgetron_URL https://github.com/gadgetron/gadgetron )
-  set(Gadgetron_TAG master)
+  set(DEFAULT_Gadgetron_URL https://github.com/gadgetron/gadgetron )
+  set(DEFAULT_Gadgetron_TAG master)
 
   ## ISMRMRD
-  set(ISMRMRD_URL https://github.com/ismrmrd/ismrmrd )
-  set(ISMRMRD_TAG master)
+  set(DEFAULT_ISMRMRD_URL https://github.com/ismrmrd/ismrmrd )
+  set(DEFAULT_ISMRMRD_TAG master)
 
 else()
-  set(SIRF_TAG v0.9.0)
+  set(DEFAULT_SIRF_TAG v0.9.0)
   ## STIR
-  set(STIR_URL https://github.com/CCPPETMR/STIR )
-  set(STIR_TAG 8bf37d9d7fdde7cb3a98a6f848d93827dbd98a18)
+  set(DEFAULT_STIR_URL https://github.com/CCPPETMR/STIR )
+  set(DEFAULT_STIR_TAG 8bf37d9d7fdde7cb3a98a6f848d93827dbd98a18)
 
   ## Gadgetron
-  set(Gadgetron_URL https://github.com/CCPPETMR/gadgetron )
-  set(Gadgetron_TAG f03829ef45e57466829e6ec46da7a7cf61db1c8a)
+  set(DEFAULT_Gadgetron_URL https://github.com/CCPPETMR/gadgetron )
+  set(DEFAULT_Gadgetron_TAG f03829ef45e57466829e6ec46da7a7cf61db1c8a)
 
   ## ISMRMRD
-  set(ISMRMRD_URL https://github.com/CCPPETMR/ismrmrd )
-  set(ISMRMRD_TAG 35012c6c8000616546c2d6b1757eba0c5b21b2d4)
+  set(DEFAULT_ISMRMRD_URL https://github.com/CCPPETMR/ismrmrd )
+  set(DEFAULT_ISMRMRD_TAG 35012c6c8000616546c2d6b1757eba0c5b21b2d4)
 
 endif()
-option(GIT_TAG_SIRF "git tag for SIRF" ${SIRF_TAG})
-mark_as_advanced(STIR_URL STIR_TAG Gadgetron_URL Gadgetron_TAG ISMRMRD_URL ISMRMRD_TAG)
+
+
+# Set the tags for SIRF, STIR, Gadgetron and ISMRMRD
+# these can be overridden by the user
+SET(SIRF_TAG ${DEFAULT_SIRF_TAG} CACHE STRING ON)
+ 
+SET(STIR_TAG ${DEFAULT_STIR_TAG} CACHE STRING ON)
+SET(STIR_URL ${DEFAULT_STIR_URL} CACHE STRING ON)
+
+SET(Gadgetron_TAG ${DEFAULT_Gadgetron_TAG} CACHE STRING ON)
+SET(Gadgetron_URL ${DEFAULT_Gadgetron_URL} CACHE STRING ON)
+
+SET(ISMRMRD_TAG ${DEFAULT_ISMRMRD_TAG} CACHE STRING ON)
+SET(ISMRMRD_URL ${DEFAULT_ISMRMRD_URL} CACHE STRING ON)
+
+mark_as_advanced(SIRF_TAG STIR_URL STIR_TAG Gadgetron_URL Gadgetron_TAG ISMRMRD_URL ISMRMRD_TAG)
 
