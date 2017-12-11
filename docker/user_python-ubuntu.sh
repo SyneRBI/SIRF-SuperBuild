f785cee26efb618587f9a7075dd92cee2f414db5
Casper da Costa-Luis <casper.dcl@physics.org>
2017-12-11 16:44:19 +0000

    python requirements
    as discussed in https://github.com/CCPPETMR/SIRF/pull/69

--- docker/user_python-ubuntu.sh
+++ docker/user_python-ubuntu.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 # Python (virtualenv)
 curl -O https://bootstrap.pypa.io/get-pip.py
-sudo -H python get-pip.py
+python get-pip.py --user
 rm get-pip.py
-sudo -H python -m pip install -U pip virtualenv
-virtualenv ~/py2
+python -m pip install -U --user pip virtualenv
+python -m virtualenv ~/py2
 export PIPINST="$HOME/py2/bin/pip install -U"
 
 # Python (runtime)
-$PIPINST docopt
-#$PIPINST cython
-$PIPINST matplotlib
-#PIPINST scipy
-git clone https://github.com/ismrmrd/ismrmrd-python-tools.git
-$PIPINST ./ismrmrd-python-tools
+$PIPINST -r requirements.txt
