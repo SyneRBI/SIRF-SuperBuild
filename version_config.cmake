360403c399cf87c170117910e9cba6048a9604e7
Kris Thielemans <kris.f.thielemans@hotmail.com>
2017-12-20 12:28:22 +0000

    upgrade to HDF5 1.10.1

--- version_config.cmake
+++ version_config.cmake
@@ -47,8 +47,13 @@ set(FFTW3double_URL ${FFTW3_URL})
 set(FFTW3double_MD5 ${FFTW3_MD5})
 
 ## HDF5
-set(HDF5_URL https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.0-patch1/src/CMake-hdf5-1.10.0-patch1.tar.gz )
-set(HDF5_MD5 6fb456d03a60f358f3c077288a6d1cd8 )
+#if (WIN32)
+  set(HDF5_URL https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.1/src/hdf5-1.10.1.tar.gz)
+  set(HDF5_MD5 43a2f9466702fb1db31df98ae6677f15 )
+#else()
+#  set(HDF5_URL https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.0-patch1/src/CMake-hdf5-1.10.0-patch1.tar.gz )
+#  set(HDF5_MD5 6fb456d03a60f358f3c077288a6d1cd8 )
+#endif()
 
 ## SWIG
 if (WIN32)
