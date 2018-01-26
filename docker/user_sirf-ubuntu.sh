4a2da00d25352eb68b8a5fe67d334f8ff2b192cc
Casper da Costa-Luis <casper.dcl@physics.org>
2018-01-26 15:39:19 +0000

    SuperBuild unify branch merge build fix for docker, .mailmap

--- docker/user_sirf-ubuntu.sh
+++ docker/user_sirf-ubuntu.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # SIRF
-git clone https://github.com/CCPPETMR/SIRF-SuperBuild --recursive \
-  -b system-build
+git clone https://github.com/CCPPETMR/SIRF-SuperBuild --recursive -b master
 pushd SIRF-SuperBuild
 cmake $EXTRA_BUILD_FLAGS .
 make -j 2
