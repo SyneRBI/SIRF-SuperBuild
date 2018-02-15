be818f03b693de5ad5945d588bd81346ca3a36b3
Edoardo Pasca <edo.paskino@gmail.com>
2018-02-15 14:05:38 +0000

    added python-nose as prerequisite

--- scripts/INSTALL_prerequisites_with_apt-get.sh
+++ scripts/INSTALL_prerequisites_with_apt-get.sh
@@ -21,7 +21,7 @@ $SUDO apt-get install -y --no-install-recommends swig
 
 # TODO replace with pip install
 echo "Installing python libraries etc"
-$SUDO apt-get install -y --no-install-recommends  python-scipy python-docopt  python-numpy python-h5py python-matplotlib python-libxml2 python-psutil spyder python-tk
+$SUDO apt-get install -y --no-install-recommends  python-scipy python-docopt  python-numpy python-h5py python-matplotlib python-libxml2 python-psutil spyder python-tk python-nose
 
 
 # could do recent version of CMake
