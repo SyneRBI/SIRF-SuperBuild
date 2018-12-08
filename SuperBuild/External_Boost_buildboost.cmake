135f25b51c6092e9b55eda100c29bd9074a6fa7e
Kris Thielemans <k.thielemans@ucl.ac.uk>
2018-12-08 17:23:48 +0000

    Also build the unit_test framework
    ISMRMRD searches for unit_test. As we don't build it, it might find
    a system version, which would then create conflicts between boost versions

--- SuperBuild/External_Boost_buildboost.cmake
+++ SuperBuild/External_Boost_buildboost.cmake
@@ -23,7 +23,7 @@ if(WIN32)
   # TODO: would be better to use a variable but for some reason KT cannot pass the variable to the execute_process
   # without strange error messages of b2
   
-  execute_process(COMMAND ./b2 --with-system --with-filesystem --with-thread --with-program_options --with-chrono --with-date_time --with-atomic  --with-timer install --prefix=${BOOST_INSTALL_DIR}
+  execute_process(COMMAND ./b2 --with-system --with-filesystem --with-thread --with-program_options --with-chrono --with-date_time --with-atomic  --with-timer --with-test install --prefix=${BOOST_INSTALL_DIR}
     WORKING_DIRECTORY ${BUILD_DIR} RESULT_VARIABLE build_result)
 
 else(WIN32)
