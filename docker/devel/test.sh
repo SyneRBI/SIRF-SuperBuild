acb0088b93d1fea31ff0fff6e8fb122e18a2339e
Casper da Costa-Luis <casper.dcl@physics.org>
2019-02-28 18:24:10 +0000

    minor test tidy

--- docker/devel/test.sh
+++ docker/devel/test.sh
@@ -19,11 +19,12 @@ GCONFIG=./INSTALL/share/gadgetron/config/gadgetron.xml
 [ -f "$GCONFIG" ] || cp "$GCONFIG".example "$GCONFIG"
 ./INSTALL/bin/gadgetron >& gadgetron.log&
 
-# print for debugging
-[ "$DEBUG" != 0 ] && cat builds/SIRF/build/CMakeCache.txt
 ctest -VV
 ret=$?
+kill -n 15 $(pidof gadgetron)
+
 # print for debugging
+[ "$DEBUG" != 0 ] && cat builds/SIRF/build/CMakeCache.txt
 [ "$DEBUG" != 0 ] && cat builds/SIRF/build/Testing/Temporary/LastTest.log
 # may exceed 4MB travis log limit
 [ "$DEBUG" != 0 ] && cat gadgetron.log
