6563e5ee60963b71370a41210c535ef50fb8fd45
Casper da Costa-Luis <casper.dcl@physics.org>
2019-03-01 01:05:48 +0000

    fix gadgetron from source

--- docker/build_gadgetron-ubuntu.sh
+++ docker/build_gadgetron-ubuntu.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 set -ev
 # Gadgetron
+# from https://github.com/gadgetron/gadgetron/blob/master/docker/base/ubuntu_1804/Dockerfile#L8
 apt-get update -qq
 apt-get install -yq --no-install-recommends \
-  h5utils              \
-  liblapack-dev        \
-  libace-dev
+  libhdf5-serial-dev     \
+  libboost-all-dev       \
+  libfftw3-dev           \
+  h5utils                \
+  jq                     \
+  hdf5-tools             \
+  libatlas-base-dev      \
+  libxml2-dev            \
+  libfreetype6-dev       \
+  libxslt-dev            \
+  libarmadillo-dev       \
+  libace-dev             \
+  liblapack-dev          \
+  liblapacke-dev         \
+  libplplot-dev          \
+  libdcmtk-dev
 apt-get clean
--- docker/build_system-ubuntu.sh
+++ docker/build_system-ubuntu.sh
@@ -3,25 +3,11 @@ set -ev
 # SIRF external dependencies
 apt-get update -qq
 apt-get install -yq --no-install-recommends \
-  hdf5-tools           \
   hdfview              \
-  libarmadillo-dev     \
   libboost-all-dev     \
-  libfftw3-dev         \
   libgtest-dev         \
-  libhdf5-serial-dev   \
-  libplplot-dev        \
-  libxml2-dev          \
-  libxslt-dev          \
   swig
 apt-get clean
 
 # Not required (yet) by SIRF
-# libxml2-dev
-# libxslt-dev
-# python-h5py
-# python-libxml2
-# python-psutil
-# libplplot-dev
 # libopenblas-dev
-# libatlas-base-dev
