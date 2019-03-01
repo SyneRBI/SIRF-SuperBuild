3fff29474f05338e7b265e947363ceebe6da0e6d
Casper da Costa-Luis <casper.dcl@physics.org>
2019-03-01 13:01:08 +0000

    prepare for possibly external gadgetron

--- docker/user_sirf-ubuntu.sh
+++ docker/user_sirf-ubuntu.sh
@@ -4,11 +4,14 @@ set -ev
 # SIRF
 git clone https://github.com/CCPPETMR/SIRF-SuperBuild --recursive -b docker /opt/SIRF-SuperBuild
 pushd /opt/SIRF-SuperBuild
+
 echo $BUILD_FLAGS $EXTRA_BUILD_FLAGS
 cmake $BUILD_FLAGS $EXTRA_BUILD_FLAGS .
 make -j 2
 
 [ -f INSTALL/share/gadgetron/config/gadgetron.xml ] || \
+[ -f INSTALL/share/gadgetron/config/gadgetron.xml.example ] && \
   mv INSTALL/share/gadgetron/config/gadgetron.xml.example \
      INSTALL/share/gadgetron/config/gadgetron.xml
+
 popd
