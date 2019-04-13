6801e65e8f371cf2b9d662a51c29dfc84c8ad425
Ben Thomas <ben.thomas1984@gmail.com>
2019-04-13 12:40:06 +0100

    Configures RDP for 18.04

--- cloud/terraform/azure/scripts/install_rdp.sh
+++ cloud/terraform/azure/scripts/install_rdp.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-sudo apt-get install -y ubuntu-desktop spyder
+sudo apt-get install -y gnome spyder xrdp
 
 mkdir ~/Downloads
 cd ~/Downloads
-wget https://ccppetmrfiles.blob.core.windows.net/misc/install-xrdp-1.9.1.sh
-chmod +x install-xrdp-1.9.1.sh
-#sudo bash install-xrdp-1.9.1.sh
+
+head -n -2 /etc/xrdp/startwm.sh > startwm.sh
+echo -e "gnome-session\n" >> startwm.sh
+sudo mv startwm.sh /etc/xrdp/startwm.sh
+chmod 755 /etc/xrdp/startwm.sh
+
+touch 45-allow-colord.pkla
+echo "[Allow Colord all Users]" >> 45-allow-colord.pkla
+echo "Identity=unix-user:*" >> 45-allow-colord.pkla
+echo "Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile" >> 45-allow-colord.pkla
+echo "ResultAny=no" >> 45-allow-colord.pkla
+echo "ResultInactive=no" >> 45-allow-colord.pkla
+echo "ResultActive=yes" >> >> 45-allow-colord.pkla
+sudo mv 45-allow-colord.pkla /etc/polkit-1/localauthority/50-local.d/
+chmod 755 /etc/polkit-1/localauthority/50-local.d/45-allow-colord.pkla
