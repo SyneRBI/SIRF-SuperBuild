209d6da2d24f7c9af8a9c1ae60871154ed7f41bd
Evgueni Ovtchinnikov <me@example.com>
2019-05-03 14:57:47 +0000

    updated CHANGES.md and version_config.cmake

--- version_config.cmake
+++ version_config.cmake
@@ -139,7 +139,7 @@ if (DEVEL_BUILD)
   set(DEFAULT_ACE_TAG origin/master)
 
 else()
-  set(DEFAULT_SIRF_TAG v2.0.0-rc.1)
+  set(DEFAULT_SIRF_TAG v2.0.0)
 
   ## STIR
   set(DEFAULT_STIR_URL https://github.com/UCL/STIR )
