18e290f651dbeca30a178aba2e677b665fee94ba
Edoardo Pasca <edo.paskino@gmail.com>
2019-05-09 10:14:42 +0100

    updated SIRF version

--- version_config.cmake
+++ version_config.cmake
@@ -139,7 +139,7 @@ if (DEVEL_BUILD)
   set(DEFAULT_ACE_TAG origin/master)
 
 else()
-  set(DEFAULT_SIRF_TAG v2.0.0-rc.2)
+  set(DEFAULT_SIRF_TAG v2.0.0-rc.3)
 
   ## STIR
   set(DEFAULT_STIR_URL https://github.com/UCL/STIR )
