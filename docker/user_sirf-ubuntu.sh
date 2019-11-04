6f6b44909f684eb071cf66d4835d41d31ad99085
Casper da Costa-Luis <casper.dcl@physics.org>
2019-11-04 16:45:50 +0000

    silly docker fix

--- docker/user_sirf-ubuntu.sh
+++ docker/user_sirf-ubuntu.sh
@@ -6,7 +6,7 @@ INSTALL_DIR="${1:-/opt}"
 git clone https://github.com/CCPPETMR/SIRF-SuperBuild --recursive -b docker-ccache $INSTALL_DIR/SIRF-SuperBuild
 pushd $INSTALL_DIR/SIRF-SuperBuild
 
-COMPILER_FLAGS="-D CMAKE_C_COMPILER='$(which gcc)' -D CMAKE_CXX_COMPILER='$(which g++)'"
+COMPILER_FLAGS="-DCMAKE_C_COMPILER='$(which gcc)' -DCMAKE_CXX_COMPILER='$(which g++)'"
 echo $PATH
 echo $COMPILER_FLAGS
 echo $BUILD_FLAGS $EXTRA_BUILD_FLAGS
