d2f6645c76a461083682af0d844a620ee3192111
Casper da Costa-Luis <casper.dcl@physics.org>
2019-11-04 16:06:50 +0000

    docker-core stage separation

--- docker/user_sirf-ubuntu.sh
+++ docker/user_sirf-ubuntu.sh
@@ -6,12 +6,11 @@ INSTALL_DIR="${1:-/opt}"
 git clone https://github.com/CCPPETMR/SIRF-SuperBuild --recursive -b docker-ccache $INSTALL_DIR/SIRF-SuperBuild
 pushd $INSTALL_DIR/SIRF-SuperBuild
 
-#echo $PATH
-echo $BUILD_FLAGS $EXTRA_BUILD_FLAGS
 COMPILER_FLAGS="-D CMAKE_C_COMPILER='$(which gcc)' -D CMAKE_CXX_COMPILER='$(which g++)'"
+echo $PATH
 echo $COMPILER_FLAGS
+echo $BUILD_FLAGS $EXTRA_BUILD_FLAGS
 cmake $BUILD_FLAGS $EXTRA_BUILD_FLAGS $COMPILER_FLAGS .
-#PATH="$(dirname $(which gcc)):$PATH" cmake $BUILD_FLAGS $EXTRA_BUILD_FLAGS .
 
 make -j 2
 
