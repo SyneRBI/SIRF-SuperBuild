a537d5c3160dcd24a223ab91cbd40e1dd58c5285
Casper da Costa-Luis <casper.dcl@physics.org>
2019-11-04 15:49:35 +0000

    docker ccache

--- docker/user_sirf-ubuntu.sh
+++ docker/user_sirf-ubuntu.sh
@@ -6,8 +6,13 @@ INSTALL_DIR="${1:-/opt}"
 git clone https://github.com/CCPPETMR/SIRF-SuperBuild --recursive -b master $INSTALL_DIR/SIRF-SuperBuild
 pushd $INSTALL_DIR/SIRF-SuperBuild
 
+#echo $PATH
 echo $BUILD_FLAGS $EXTRA_BUILD_FLAGS
-cmake $BUILD_FLAGS $EXTRA_BUILD_FLAGS .
+COMPILER_FLAGS="-D CMAKE_C_COMPILER='$(which gcc)' -D CMAKE_CXX_COMPILER='$(which g++)'"
+echo $COMPILER_FLAGS
+cmake $BUILD_FLAGS $EXTRA_BUILD_FLAGS $COMPILER_FLAGS .
+#PATH="$(dirname $(which gcc)):$PATH" cmake $BUILD_FLAGS $EXTRA_BUILD_FLAGS .
+
 make -j 2
 
 [ -f INSTALL/share/gadgetron/config/gadgetron.xml ] || \
