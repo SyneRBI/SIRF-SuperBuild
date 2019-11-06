67e1988e8948b44c832c6e3c53dd184aa358efb0
Kris Thielemans <k.thielemans@ucl.ac.uk>
2019-11-06 12:35:14 +0000

    set SIRF_TAG to v2.1.0-rc.3

--- version_config.cmake
+++ version_config.cmake
@@ -187,7 +187,7 @@ if (DEVEL_BUILD)
   set(DEFAULT_CCPi-Astra_TAG origin/master)
 
 else()
-  set(DEFAULT_SIRF_TAG v2.1.0-rc.2)
+  set(DEFAULT_SIRF_TAG v2.1.0-rc.3)
 
   ## STIR
   set(DEFAULT_STIR_URL https://github.com/UCL/STIR )
