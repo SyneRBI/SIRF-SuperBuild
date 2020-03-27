674f69083a5b9b44c4f3ec6318b8d96d811be4c9
Edoardo Pasca <edo.paskino@gmail.com>
2020-03-27 17:12:19 +0000

    updated with macros

--- SuperBuild/External_JSON.cmake
+++ SuperBuild/External_JSON.cmake
@@ -30,11 +30,9 @@ ExternalProject_Include_Dependencies(${proj} DEPENDS_VAR ${proj}_DEPENDENCIES)
 # Set external name (same as internal for now)
 set(externalProjName ${proj})
 
-set(${proj}_SOURCE_DIR "${SOURCE_ROOT_DIR}/${proj}" )
-set(${proj}_BINARY_DIR "${SUPERBUILD_WORK_DIR}/builds/${proj}/build" )
-set(${proj}_DOWNLOAD_DIR "${SUPERBUILD_WORK_DIR}/downloads/${proj}" )
-set(${proj}_STAMP_DIR "${SUPERBUILD_WORK_DIR}/builds/${proj}/stamp" )
-set(${proj}_TMP_DIR "${SUPERBUILD_WORK_DIR}/builds/${proj}/tmp" )
+SetCanonicalDirectoryNames(${proj})
+# Get any flag from the superbuild call that may be particular to this projects CMAKE_ARGS
+SetExternalProjectFlags(${proj})
 
 if(NOT ( DEFINED "USE_SYSTEM_${externalProjName}" AND "${USE_SYSTEM_${externalProjName}}" ) )
   message(STATUS "${__indent}Adding project ${proj}")
@@ -54,26 +52,10 @@ if(NOT ( DEFINED "USE_SYSTEM_${externalProjName}" AND "${USE_SYSTEM_${externalPr
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     ${${proj}_EP_ARGS_GIT}
-    SOURCE_DIR ${${proj}_SOURCE_DIR}
-    BINARY_DIR ${${proj}_BINARY_DIR}
-    DOWNLOAD_DIR ${${proj}_DOWNLOAD_DIR}
-    STAMP_DIR ${${proj}_STAMP_DIR}
-    TMP_DIR ${${proj}_TMP_DIR}
-    INSTALL_DIR ${libcilreg_Install_Dir}
-    # apparently this is the only way to pass environment variables to
-    # external projects
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env CIL_VERSION=${${proj}_TAG}
-    ${CMAKE_COMMAND} ${${proj}_SOURCE_DIR}
-        -DCMAKE_INSTALL_PREFIX:STRING=${${proj}_Install_Dir}}
-        -DCMAKE_BUILD_TYPE:STRING=Release
-        
-    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install
+    ${${proj}_EP_ARGS_DIRS}
     DEPENDS ${${proj}_DEPENDENCIES}
   )
 
-
-  set(${proj}_ROOT        ${${proj}_SOURCE_DIR})
-  set(${proj}_INCLUDE_DIR ${${proj}_SOURCE_DIR})
 
   else()
     ExternalProject_Add_Empty(${proj} DEPENDS "${${proj}_DEPENDENCIES}"
