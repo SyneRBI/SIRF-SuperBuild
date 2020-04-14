a844047b5bff7895a3c2aa57b02ec8e133fea325
Edoardo Pasca <edo.paskino@gmail.com>
2020-04-14 02:46:24 +0000

    update headers

--- SuperBuild/External_Boost_configureboost.cmake
+++ SuperBuild/External_Boost_configureboost.cmake
@@ -1,8 +1,8 @@
 #========================================================================
 # Author: Benjamin A Thomas
-# Copyright 2017 University College London
+# Copyright 2017, 2020 University College London
 #
-# This file is part of the CCP SynerBI Synergistic Image Reconstruction Framework (SIRF) SuperBuild.
+# This file is part of the CCP SyneRBI (formely PETMR) Synergistic Image Reconstruction Framework (SIRF) SuperBuild.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- SuperBuild/External_ITK.cmake
+++ SuperBuild/External_ITK.cmake
@@ -2,10 +2,9 @@
 # Author: Richard Brown
 # Author: Benjamin A Thomas
 # Author: Kris Thielemans
-# Copyright 2017 University College London
-# Copyright 2017 STFC
+# Copyright 2017, 2020 University College London
 #
-# This file is part of the CCP SynerBI Synergistic Image Reconstruction Framework (SIRF) SuperBuild.
+# This file is part of the CCP SyneRBI (formely PETMR) Synergistic Image Reconstruction Framework (SIRF) SuperBuild.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
