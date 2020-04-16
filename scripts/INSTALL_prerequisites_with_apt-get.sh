d3ceb674edafa781250a672cec5ebce58a6bb9a4
Edoardo Pasca <edo.paskino@gmail.com>
2020-04-16 04:22:49 +0100

    install linux kernel headers (#154)
    * install linux kernel headers 
    
    closes  #149
    
    * move build-essential at linux-headers install time

--- scripts/INSTALL_prerequisites_with_apt-get.sh
+++ scripts/INSTALL_prerequisites_with_apt-get.sh
@@ -11,10 +11,14 @@ set -e
 #if [ -z "SUDO" ]; then SUDO=sudo; fi
 export DEBIAN_FRONTEND=noninteractive
 SUDO=sudo
-$SUDO apt update
+
+APT_GET_INSTALL="$SUDO apt-get install -y --no-install-recommends"
+
+echo "Installing linux headers for VGA updates"
+${APT_GET_INSTALL} build-essential linux-headers-$(uname -r)
+
 echo "Installing Gadgetron pre-requisites..."
-APT_GET_INSTALL="$SUDO apt install -y --no-install-recommends"
-${APT_GET_INSTALL} libhdf5-serial-dev git build-essential libfftw3-dev h5utils hdf5-tools \
+${APT_GET_INSTALL} libhdf5-serial-dev git libfftw3-dev h5utils hdf5-tools \
 	liblapack-dev libarmadillo-dev libace-dev libgtest-dev libopenblas-dev libpugixml-dev \
 	libatlas-base-dev libatlas-base-dev libxml2-dev libxslt1-dev unzip
 
