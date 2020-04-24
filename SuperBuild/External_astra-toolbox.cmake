c391129199e4e7e7c87e0f6c3c3faee4c992d874
Casper da Costa-Luis <casper.dcl@physics.org>
2020-04-24 13:55:13 +0100

    formely => formerly

--- SuperBuild/External_astra-toolbox.cmake
+++ SuperBuild/External_astra-toolbox.cmake
@@ -2,7 +2,7 @@
 # Author: Edoardo Pasca
 # Copyright 2019, 2020 STFC
 #
-# This file is part of the CCP SyneRBI (formely PETMR) Synergistic Image Reconstruction Framework (SIRF) SuperBuild.
+# This file is part of the CCP SyneRBI (formerly PETMR) Synergistic Image Reconstruction Framework (SIRF) SuperBuild.
 ##
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
