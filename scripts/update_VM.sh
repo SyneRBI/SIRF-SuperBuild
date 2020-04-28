a29c0debb58d0ce8db7ed3c8388f28b0c0d23265
Kris Thielemans <KrisThielemans@users.noreply.github.com>
2020-04-28 14:23:00 +0100

    Update update_VM.sh
    cd to directory before changing git remote

--- scripts/update_VM.sh
+++ scripts/update_VM.sh
@@ -4,6 +4,7 @@ echo "Updating..."
 if [ -d $userHOME/devel/CCPPETMR_VM ]; then
   if [ ! -h $userHOME/devel/SyneRBI_VM ]; then
     ln -s $userHOME/devel/CCPPETMR_VM $userHOME/devel/SyneRBI_VM
+    cd $userHOME/devel/CCPPETMR_VM
     git remote set-url origin https://github.com/SyneRBI/SyneRBI_VM.git
   fi
 fi
