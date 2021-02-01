9b8cf774f74d15f9fc137000964bdea73ea34a23
Edoardo Pasca <edo.paskino@gmail.com>
2021-02-01 13:15:58 +0000

    [ci skip] fix codacy warning

--- patches/Gadgetron_include-patch.py
+++ patches/Gadgetron_include-patch.py
@@ -22,5 +22,5 @@ with open(sys.argv[1], 'r') as f:
     testdata = f.read()
     updated = testdata.replace('../NFFT.h', "NFFT.h")
     updated = updated.replace('../nfft_export.h', "nfft_export.h")
-with open(sys.argv[2], 'w') as fw:    
+with open(sys.argv[2], 'w') as fw:
     fw.write(updated)
