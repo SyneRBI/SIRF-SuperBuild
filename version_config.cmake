ab6212f490c5d7dd6758a1ef6b3664898d7f5ec5
Edoardo Pasca <edo.paskino@gmail.com>
2021-04-28 14:29:13 +0100

    update CIL to 21.1.0 version

--- version_config.cmake
+++ version_config.cmake
@@ -164,7 +164,7 @@ set(DEFAULT_JSON_TAG v3.9.1)
 
 # CCPi CIL
 set(DEFAULT_CCPi-Framework_URL https://github.com/vais-ral/CCPi-Framework.git)
-set(DEFAULT_CCPi-Framework_TAG "v21.0.0")
+set(DEFAULT_CCPi-Framework_TAG "v21.1.0")
 set(DEFAULT_CCPi-Astra_URL https://github.com/vais-ral/CCPi-Astra.git)
 set(DEFAULT_CCPi-Astra_TAG "v20.11.2")
 set(DEFAULT_CCPi-Regularisation-Toolkit_URL https://github.com/vais-ral/CCPi-Regularisation-Toolkit.git)
