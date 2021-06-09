9d9e0ed80e28d3a2570ac430da9eb2c2fafc4ecf
Edoardo Pasca <edo.paskino@gmail.com>
2021-06-09 18:33:45 +0100

    restored old entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@
 # Either use runtime USER_ID:GROUP_ID or fallback 1000:1000
 
 USER_ID=${USER_ID:-1000}
-GROUP_ID=${GROUP_ID:-100}
+GROUP_ID=${GROUP_ID:-1000}
 mainUser=${mainUser:-sirfuser}
 OLD_HOME=/home-away
 export HOME=/home/$mainUser
@@ -16,25 +16,25 @@ fi
 
 cd /
 # allow docker exec into already started container
-[ -d $OLD_HOME ] && sudo mv $OLD_HOME $HOME
+[ -d $OLD_HOME ] && mv $OLD_HOME $HOME
 cd $HOME
 
 echo "Creating $mainUser:$USER_ID:$GROUP_ID"
 # groupadd -g $GROUP_ID -o -f $mainUser
-sudo addgroup --quiet --system --gid "$GROUP_ID" "$mainUser"
+addgroup --quiet --system --gid "$GROUP_ID" "$mainUser"
 # useradd --shell /bin/bash -u $USER_ID -o -c "" -M -d $HOME \
 #   -g $mainUser -G sudo $mainUser \
 #   -p $(echo somepassword | openssl passwd -1 -stdin)
-sudo adduser --quiet --system --shell /bin/bash \
+adduser --quiet --system --shell /bin/bash \
   --no-create-home --home /home/"$mainUser" \
   --ingroup "$mainUser" --uid "$USER_ID" "$mainUser"
 
-sudo echo "$mainUser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/"$mainUser"
+echo "$mainUser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/"$mainUser"
 
 for i in /opt/* "$HOME"; do
   if [ -d "$i" ]; then
     echo "Updating file permissions for $i"
-    sudo chown -R $mainUser:$mainUser "$i"
+    chown -R $mainUser:$mainUser "$i"
   fi
 done
 
