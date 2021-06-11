9ef7481d48edda830fd37747a7a659846b880d1c
Edoardo Pasca <edo.paskino@gmail.com>
2021-06-11 16:21:21 +0100

    remove installation of system hdf5
    the hdf5 library that is found by Gadgetron is in the conda environment

--- docker/build_gadgetron-ubuntu.sh
+++ docker/build_gadgetron-ubuntu.sh
@@ -4,16 +4,13 @@ set -ev
 # from https://github.com/gadgetron/gadgetron/blob/master/docker/base/ubuntu_1804/Dockerfile#L8
 apt-get update -qq
 apt-get install -yq --no-install-recommends \
-  libhdf5-serial-dev     \
   libboost-dev libboost-chrono-dev \
         libboost-filesystem-dev libboost-thread-dev \
         libboost-date-time-dev libboost-regex-dev \
         libboost-program-options-dev libboost-atomic-dev \
         libboost-test-dev libboost-timer-dev \
   libfftw3-dev           \
-  h5utils                \
   jq                     \
-  hdf5-tools             \
   libatlas-base-dev      \
   libxml2-dev            \
   libfreetype6-dev       \
