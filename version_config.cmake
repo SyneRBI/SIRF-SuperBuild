018f2226602e1a079778f775db659553471aee8d
Kris Thielemans <k.thielemans@ucl.ac.uk>
2021-06-18 16:41:13 +0000

    set SIRF_TAG to v3.1.0-rc.1

--- version_config.cmake
+++ version_config.cmake
@@ -190,8 +190,7 @@ if (DEVEL_BUILD)
   set(DEFAULT_CCPi-Regularisation-Toolkit_TAG origin/master)
 
 else()
-  set(DEFAULT_SIRF_TAG 17f7fa91704b0b5309d1e2562489aaf7932875e8)
-#  set(DEFAULT_SIRF_TAG v3.0.0)
+  set(DEFAULT_SIRF_TAG v3.1.0-rc.1)
 
   ## STIR
   set(DEFAULT_STIR_URL https://github.com/UCL/STIR )
