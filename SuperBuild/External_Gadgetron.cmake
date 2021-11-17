6b7a00770594598899fe91dfc2be8d82a0732bd9
Edoardo Pasca <edo.paskino@gmail.com>
2021-11-17 09:55:32 +0000

    do not build testing for gadgetron

--- SuperBuild/External_Gadgetron.cmake
+++ SuperBuild/External_Gadgetron.cmake
@@ -130,7 +130,7 @@ if(NOT ( DEFINED "USE_SYSTEM_${externalProjName}" AND "${USE_SYSTEM_${externalPr
       -DUSE_MKL:BOOL=${${proj}_USE_MKL}
       -DUSE_CUDA:BOOL=${${proj}_USE_CUDA}
       -DUSE_OPENMP:BOOL=${${proj}_ENABLE_OPENMP}
-      -DBUILD_TESTING:BOOL=ON
+      -DBUILD_TESTING:BOOL=OFF
       )
 
   if (CBLAS_INCLUDE_DIR)
