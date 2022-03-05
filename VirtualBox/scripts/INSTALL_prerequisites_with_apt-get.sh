b5c099a004644d1f0a7b2c7a65c6a6f7fb5f12c4
Kris Thielemans <k.thielemans@ucl.ac.uk>
2022-03-05 19:51:23 +0000

    reduce overlap between VirtualBox and docker scripts
    Most VM scripts are now calling docker docker scripts for installing
    dependencies.
    
    I've tried not to drop any installations.

--- VirtualBox/scripts/INSTALL_prerequisites_with_apt-get.sh
+++ VirtualBox/scripts/INSTALL_prerequisites_with_apt-get.sh
@@ -12,73 +12,23 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 SUDO=sudo
 
+# TODO would be better to guarantee absolute path for SCRIPTS
+SCRIPTS=`dirname $0`/../../docker
+
+$SUDO $SCRIPTS/build_essential-ubuntu.sh
+
 APT_GET_INSTALL="$SUDO apt-get install -y --no-install-recommends"
 
-${APT_GET_INSTALL} build-essential
 echo "Installing linux headers for VGA updates"
 # By appending the "true" we make sure that the script does not stop
 # if this install failes (e.g. on Windows WSL2)
 ${APT_GET_INSTALL} linux-headers-$(uname -r) || true
 
+$SUDO $SCRIPTS/build_system-ubuntu.sh
+
 echo "Installing Gadgetron pre-requisites..."
-${APT_GET_INSTALL} libhdf5-serial-dev git libfftw3-dev h5utils hdf5-tools \
-	liblapack-dev liblapacke-dev libarmadillo-dev libace-dev libgtest-dev libopenblas-dev libpugixml-dev \
-	libatlas-base-dev libatlas-base-dev libxml2-dev libxslt1-dev unzip \
-	libdcmtk-dev
-
-echo "Installing boost 1.65 or later"
-# first find current boost version (if any)
-# the 'tail' makes sure we use the last one listed by apt-cache in case there is more than 1 version
-function find_boost_version() {
-  tmp=`apt-cache search libboost|grep ALL|egrep libboost[1-9]|tail -n 1`
-  boost_major=${tmp:8:1}
-  boost_minor=${tmp:10:2}
-}
-
-find_boost_version
-
-echo "Found Boost major version ${boost_major}, minor ${boost_minor}"
-if [ $boost_major -gt 1 -o $boost_minor -gt 64 ]
-then
-    echo "installing Boost ${boost_major}.${boost_minor} from system apt"
-    ${APT_GET_INSTALL} libboost-dev libboost-chrono-dev \
-        libboost-filesystem-dev libboost-thread-dev \
-        libboost-date-time-dev libboost-regex-dev \
-        libboost-program-options-dev libboost-atomic-dev \
-        libboost-test-dev libboost-timer-dev
-else    
-    # packaged boost is too old
-    # we need to find a ppa that has it. This is unsafe and likely prone to falling over
-    # when the ppa is no longer maintained
-    echo "trying to find boost from ppa:mhier/libboost-latest"
-    ${APT_GET_INSTALL} software-properties-common
-    $SUDO add-apt-repository -y  ppa:mhier/libboost-latest
-    $SUDO apt update
-    # get rid of the default installed boost version
-    $SUDO apt remove -y libboost-all-dev
-    $SUDO apt auto-remove -y
-    # TODO: find out which version is in the ppa
-    find_boost_version
-    echo "installing Boost ${boost_major}.${boost_minor} from ppa:mhier apt"
-    ${APT_GET_INSTALL} libboost${boost_major}.${boost_minor}-all-dev
-fi
-
-echo "Installing SWIG..."
-
-${APT_GET_INSTALL} swig
-
-echo "Installing doxygen related packages"
-${APT_GET_INSTALL} doxygen graphviz
-
-# replaced with pip install
-#echo "Installing python libraries etc"
-#$SUDO apt-get install -y --no-install-recommends  python-scipy python-docopt  python-numpy python-h5py python-matplotlib python-libxml2 python-psutil python-tk python-nose
-
-echo "Installing glog"
-${APT_GET_INSTALL} libgoogle-glog-dev
-
-echo "Installing unzip"
-${APT_GET_INSTALL} unzip
+$SUDO $SCRIPTS/build_gadgetron-ubuntu.sh
+${APT_GET_INSTALL} libgtest-dev libopenblas-dev libpugixml-dev
 
 echo "Installing expect"
 ${APT_GET_INSTALL} expect
--- docker/build_system-ubuntu.sh
+++ docker/build_system-ubuntu.sh
@@ -2,23 +2,63 @@
 set -ev
 # SIRF external dependencies
 apt-get update -qq
-apt-get install -yq --no-install-recommends \
-  libboost-dev libboost-chrono-dev \
-  libboost-filesystem-dev libboost-thread-dev \
-  libboost-date-time-dev libboost-regex-dev \
-  libboost-program-options-dev libboost-atomic-dev \
-  libboost-test-dev libboost-timer-dev \
+APT_GET_INSTALL="apt-get install -yq --no-install-recommends"
+
+# echo "Installing boost 1.65 or later"
+# first find current boost version (if any)
+# the 'tail' makes sure we use the last one listed by apt-cache in case there is more than 1 version
+function find_boost_version() {
+  tmp=`apt-cache search libboost|grep ALL|egrep libboost[1-9]|tail -n 1`
+  boost_major=${tmp:8:1}
+  boost_minor=${tmp:10:2}
+}
+
+find_boost_version
+
+# echo "Found Boost major version ${boost_major}, minor ${boost_minor}"
+if [ $boost_major -gt 1 -o $boost_minor -gt 64 ]
+then
+    echo "installing Boost ${boost_major}.${boost_minor} from system apt"
+    ${APT_GET_INSTALL} libboost-dev libboost-chrono-dev \
+        libboost-filesystem-dev libboost-thread-dev \
+        libboost-date-time-dev libboost-regex-dev \
+        libboost-program-options-dev libboost-atomic-dev \
+        libboost-test-dev libboost-timer-dev \
+        libboost-random-dev
+else
+    # packaged boost is too old
+    # we need to find a ppa that has it. This is unsafe and likely prone to falling over
+    # when the ppa is no longer maintained
+    echo "trying to find boost from ppa:mhier/libboost-latest"
+    ${APT_GET_INSTALL} software-properties-common
+    $SUDO add-apt-repository -y  ppa:mhier/libboost-latest
+    $SUDO apt update
+    # get rid of the default installed boost version
+    $SUDO apt remove -y libboost-all-dev
+    $SUDO apt auto-remove -y
+    # TODO: find out which version is in the ppa
+    find_boost_version
+    echo "installing Boost ${boost_major}.${boost_minor} from ppa:mhier apt"
+    ${APT_GET_INSTALL} libboost${boost_major}.${boost_minor}-all-dev
+fi
+
+${APT_GET_INSTALL} \
   libfftw3-dev         \
   libhdf5-serial-dev   \
   swig
 
+# doxygen related packages
+${APT_GET_INSTALL} doxygen graphviz
+
+${APT_GET_INSTALL} libgoogle-glog-dev
+
 # siemens_to_ismrmrd external dependencies
-apt-get install -yq --no-install-recommends \
+${APT_GET_INSTALL} \
   libxml2-dev          \
   libxslt-dev
 
 # ASTRA toolbox external dependencies
-apt-get install -yq --no-install-recommends \
+${APT_GET_INSTALL} \
   autotools-dev        \
   automake             \
   autogen              \
