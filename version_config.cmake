7b930ac37eacef59189c2511247489996a0134d4
Edoardo Pasca <edo.paskino@gmail.com>
2022-03-09 21:22:08 +0000

    set boost 1.78 and bump siemens_to_ismrmrd
    After https://github.com/ismrmrd/siemens_to_ismrmrd/pull/93 bump siemens_to_ismrmrd commit and restore Boost 1.78

--- version_config.cmake
+++ version_config.cmake
@@ -29,17 +29,14 @@ if (APPLE) # really should be checking for CLang
     set(Boost_URL http://downloads.sourceforge.net/project/boost/boost/${Boost_VERSION}/boost_1_68_0.zip)
     set(Boost_MD5 f4096c4583947b0eb103c8539f1623a3)
 else()
-    # Use version in Ubuntu 20.04, i.e. 1.71
-    # but it is not in the binaries, so I try 1.72
-    # as it seems siemens_to_ismrmrd does not work with 1.78
-     set(Boost_VERSION 1.72.0)
+     set(Boost_VERSION 1.78.0)
      if (BUILD_GADGETRON)
        set(Boost_REQUIRED_VERSION 1.65.1)
      else()
        # Ubutnu 16.04 version should be fine
        set(Boost_REQUIRED_VERSION 1.58.0)
      endif()
-     set(Boost_URL http://downloads.sourceforge.net/project/boost/boost/${Boost_VERSION}/boost_1_72_0.zip)
+     set(Boost_URL http://downloads.sourceforge.net/project/boost/boost/${Boost_VERSION}/boost_1_78_0.zip)
      set(Boost_MD5 93cf8511f2e9b4456e5178cb07fc829d)
 endif()
 
@@ -184,7 +181,7 @@ if (DEVEL_BUILD)
 
   ## siemens_to_ismrmrd
   set(DEFAULT_siemens_to_ismrmrd_URL https://github.com/ismrmrd/siemens_to_ismrmrd )
-  set(DEFAULT_siemens_to_ismrmrd_TAG 6d0ab3d3d0c8ade5c0526db1c6af9825008425ad)
+  set(DEFAULT_siemens_to_ismrmrd_TAG origin/master)
 
   ## pet-rd-tools
   set(DEFAULT_pet_rd_tools_URL https://github.com/UCL/pet-rd-tools )
@@ -209,7 +206,7 @@ else()
   set(DEFAULT_ISMRMRD_TAG v1.7.0)
   ## siemens_to_ismrmrd
   set(DEFAULT_siemens_to_ismrmrd_URL https://github.com/ismrmrd/siemens_to_ismrmrd)
-  set(DEFAULT_siemens_to_ismrmrd_TAG origin/master)
+  set(DEFAULT_siemens_to_ismrmrd_TAG b87759e49e53dab4939147eb52b7a0e6465f3d04)
 
   ## pet-rd-tools
   set(DEFAULT_pet_rd_tools_URL https://github.com/UCL/pet-rd-tools )
