59fae18e482d4659d465512ebcb81fd59ed75005
Kris Thielemans <k.thielemans@ucl.ac.uk>
2022-06-11 08:08:15 +0100

    fix find_boost_version in docker/VM script
    fixes #723

--- docker/build_system-ubuntu.sh
+++ docker/build_system-ubuntu.sh
@@ -6,9 +6,8 @@ APT_GET_INSTALL="apt-get install -yq --no-install-recommends"
 
 # echo "Installing boost 1.65 or later"
 # first find current boost version (if any)
-# the 'tail' makes sure we use the last one listed by apt-cache in case there is more than 1 version
 function find_boost_version() {
-  tmp=$(apt-cache search libboost|grep ALL|egrep libboost[1-9]|tail -n 1)
+  tmp=$(apt-cache search libboost|egrep 'libboost[1-9].*-dev'|tail -n 1)
   boost_major=${tmp:8:1}
   boost_minor=${tmp:10:2}
 }
@@ -29,6 +28,9 @@ else
     # packaged boost is too old
     # we need to find a ppa that has it. This is unsafe and likely prone to falling over
     # when the ppa is no longer maintained
+    echo "Please submit a PR with a recent PPA for boost for your OS."
+    exit 1
+    # the mhier PPA is no longer supported
     echo "trying to find boost from ppa:mhier/libboost-latest"
     ${APT_GET_INSTALL} software-properties-common
     $SUDO add-apt-repository -y  ppa:mhier/libboost-latest
