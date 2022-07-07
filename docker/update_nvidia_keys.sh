f306716f4a946cbc3ee91143d5fe796ac8f5e0c3
Edoardo Pasca <edo.paskino@gmail.com>
2022-07-07 22:37:45 +0000

    implement basic try/catch

--- docker/update_nvidia_keys.sh
+++ docker/update_nvidia_keys.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
-if [ -n "${BASE_IMAGE}" ]; then 
-  bm=$BASE_IMAGE
-else
-  bm="ubuntu:18.04"
-fi
 
-if [ ${bm} = "ubuntu:18.04" ]; then 
-  apt-get update -y
-  apt-get install -yq gnupg wget
-fi
+{
+    apt-get update -y && apt-get install -yq gnupg wget &&\
+    # https://developer.nvidia.com/blog/updating-the-cuda-linux-gpg-repository-key/
+    apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
 
-# https://developer.nvidia.com/blog/updating-the-cuda-linux-gpg-repository-key/
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
+} || {
+
+    # https://developer.nvidia.com/blog/updating-the-cuda-linux-gpg-repository-key/
+    apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
+
+}
+
