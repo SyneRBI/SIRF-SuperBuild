8b6d746d3b78005820aa5a02c532a7949db0d5dc
Edoardo Pasca <edo.paskino@gmail.com>
2022-07-08 10:47:13 +0000

    remove install add-apt-repository as addressed in #734

--- docker/update_nvidia_keys.sh
+++ docker/update_nvidia_keys.sh
@@ -7,5 +7,3 @@ apt-get update -y && apt-get install -yq gnupg wget || true
 
 # https://developer.nvidia.com/blog/updating-the-cuda-linux-gpg-repository-key/
 apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
-apt-get install software-properties-common -yq
-
