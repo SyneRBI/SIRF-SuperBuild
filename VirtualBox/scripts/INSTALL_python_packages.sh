9b43c4667fa55c6a9a4dda9fe367e7cacde2d0f0
Edoardo Pasca <edo.paskino@gmail.com>
2022-08-05 14:17:23 +0100

    install setuptools

--- VirtualBox/scripts/INSTALL_python_packages.sh
+++ VirtualBox/scripts/INSTALL_python_packages.sh
@@ -72,7 +72,8 @@ ${APT_GET_INSTALL} ${PYTHON}-pip ${PYTHON}-dev
 # TODO would be better to guarantee absolute path for SCRIPTS
 SCRIPTS="$(dirname $0)/../../docker"
 
-${PYTHON} -m pip install $PIPOPTIONS -y -U -r ${SCRIPTS}/requirements.txt
+${PYTHON} -m pip install $PIPOPTIONS -U setuptools
+${PYTHON} -m pip install $PIPOPTIONS -U -r ${SCRIPTS}/requirements.txt
 # $PYTHON -m pip install $PIPOPTIONS --upgrade pip wheel setuptools
 # $PYTHON -m pip install $PIPOPTIONS --only-binary=numpy,scipy,matplotlib numpy scipy matplotlib nose coverage docopt deprecation nibabel pytest tqdm
 # $PYTHON -m pip install $PIPOPTIONS jupyter spyder
