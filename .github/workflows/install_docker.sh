fef2163654ca96938e18db3c92f32edca1df3b88
Laura Murgatroyd <60604372+lauramurgatroyd@users.noreply.github.com>
2022-08-18 19:19:54 +0100

    Github Action for Docker Builds (#748)
    * create Github action for docker builds
    
    * add paths ignore to actions
    
    Co-authored-by: Kris Thielemans <k.thielemans@ucl.ac.uk>

---
+++ .github/workflows/install_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Taken from: https://github.com/vais-ral/CILViewer/blob/master/.github/workflows/docker_build_test_publish.yml
+apt-get update -y
+apt-get install ca-certificates curl gnupg lsb-release -y
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+docker run hello-world
