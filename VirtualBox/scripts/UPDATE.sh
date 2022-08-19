4a7bc205609c5b8178154eac73d445f0abf7452d
Kris Thielemans <k.thielemans@ucl.ac.uk>
2022-08-19 02:08:46 +0200

    VM: fix apt/python installation script versions
    The installation scripts were executed before the SIRF-SuperBuild was checked-out
    to the correction version (specified with -t).
    
    To do this, the SuperBuild() bash function had to be split into 2. I've
    moved all bash functions to the top, which is probably clearer anyway.

--- VirtualBox/scripts/UPDATE.sh
+++ VirtualBox/scripts/UPDATE.sh
@@ -30,6 +30,123 @@
 #
 #=========================================================================
 
+# first some functions definitions
+# SuperBuild software (checkout appropriate version)
+install_SuperBuild_source(){
+  echo "==================== SuperBuild checkout ====================="
+  cd $SIRF_SRC_PATH
+  SB_repo=https://github.com/SyneRBI/SIRF-SuperBuild.git
+  if [ ! -d SIRF-SuperBuild ] 
+  then
+    git clone $SB_repo
+    cd SIRF-SuperBuild
+  else
+    cd SIRF-SuperBuild
+    git fetch --tags --all
+  fi
+  # go to SB_TAG
+  if [ $1 = 'default' ] 
+  then
+   # get the latest tag matching v
+   #SB_TAG=`git fetch; git for-each-ref refs/tags/v* --sort=-taggerdate --format='%(refname:short)' --count=1`
+   SB_TAG=`git tag | xargs -I@ git log --format=format:"%at @%n" -1 @ | sort | awk '{print $2}' | tail -1`
+  else
+   SB_TAG=$1
+  fi
+  clone_or_pull $SB_repo $SB_TAG
+  cd ..
+}
+
+# SuperBuild cmake
+SuperBuild_install(){
+  echo "==================== SuperBuild cmake/make ====================="
+  cd $SIRF_SRC_PATH
+  buildVM=buildVM
+  mkdir -p $buildVM
+  
+  cd $buildVM
+  cmake ../SIRF-SuperBuild \
+        -DCMAKE_INSTALL_PREFIX=${SIRF_INSTALL_PATH} \
+        -U\*_URL -U\*_TAG \
+        -DUSE_SYSTEM_SWIG=On \
+        -DUSE_SYSTEM_Boost=On \
+        -DUSE_SYSTEM_Armadillo=On \
+        -DUSE_SYSTEM_FFTW3=On \
+        -DUSE_SYSTEM_HDF5=ON \
+        -DBUILD_siemens_to_ismrmrd=On \
+        -DUSE_ITK=ON \
+        -DDEVEL_BUILD=OFF\
+        -DNIFTYREG_USE_CUDA=OFF\
+        -DBUILD_CIL=ON\
+        -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE"\
+        -DBUILD_pet_rd_tools=ON
+  cmake --build . -j${num_parallel}
+
+}
+
+# define a function to get the source
+# arguments: name_of_repo [git_ref]
+clone_or_pull()
+{
+  repoURL=$1
+  repo=`basename $1`
+  repo=${repo/.git//}
+  git_ref=${2:-master} # default to master
+  echo "======================  Getting/updating source for $repo"
+  cd $SIRF_SRC_PATH
+  if [ -d $repo ]
+  then
+    cd $repo
+    if [ $update_remote == 1 ]; then
+        git remote set-url origin $repoURL
+    fi
+    git fetch --tags --all
+  else
+    git clone --recursive $repoURL
+    cd $repo
+  fi
+  git checkout $git_ref
+  # check if we are not in detached HEAD state
+  if git symbolic-ref -q HEAD
+  then
+      # We are on a local branch.
+      echo "Warning: updating your local branch with 'git pull'"
+      git pull
+  fi
+  git submodule update --init
+}
+
+# define a function to build and install
+# arguments: name_of_repo [cmake arguments]
+build_and_install()
+{
+  repo=$1
+  shift
+  echo "======================  Building $repo"
+  cd $BUILD_PATH
+  if [ -d $repo ]
+  then
+    cd $repo
+    cmake .
+  else
+    mkdir $repo
+    cd $repo
+    cmake $* $SIRF_SRC_PATH/$repo
+  fi
+  echo "======================  Installing $repo"
+  make -j${num_parallel} install
+}
+
+# function to do everything
+update()
+{
+  clone_or_pull $1
+  build_and_install $*
+}
+
+# end of function definitions
+#=========================================================================
+# start of script
 
 # Exit if something goes wrong
 set -e
@@ -146,6 +263,9 @@ fi
 
 SIRF_INSTALL_PATH=$SIRF_SRC_PATH/install
 
+# Checkout correct version of the SuperBuild
+install_SuperBuild_source $SB_TAG
+
 # Optionally install/update pre-requisites
 if [ $apt_install == 1 ]; then
   cd "$SIRF_SRC_PATH"/SIRF-SuperBuild/VirtualBox/scripts
@@ -176,115 +296,9 @@ git config --global filter.nbstripout.extrakeys '
   metadata.celltoolbar metadata.language_info.codemirror_mode.version
   metadata.language_info.pygments_lexer metadata.language_info.version'
 
-# SuperBuild
-SuperBuild(){
-  echo "==================== SuperBuild ====================="
-  cd $SIRF_SRC_PATH
-  SB_repo=https://github.com/SyneRBI/SIRF-SuperBuild.git
-  if [ ! -d SIRF-SuperBuild ] 
-  then
-    git clone $SB_repo
-    cd SIRF-SuperBuild
-  else
-    cd SIRF-SuperBuild
-    git fetch --tags --all
-  fi
-  # go to SB_TAG
-  if [ $1 = 'default' ] 
-  then
-   # get the latest tag matching v
-   #SB_TAG=`git fetch; git for-each-ref refs/tags/v* --sort=-taggerdate --format='%(refname:short)' --count=1`
-   SB_TAG=`git tag | xargs -I@ git log --format=format:"%at @%n" -1 @ | sort | awk '{print $2}' | tail -1`
-  else
-   SB_TAG=$1
-  fi
-  clone_or_pull $SB_repo $SB_TAG
-  cd ..
-  buildVM=buildVM
-  mkdir -p $buildVM
-  
-  cd $buildVM
-  cmake ../SIRF-SuperBuild \
-        -DCMAKE_INSTALL_PREFIX=${SIRF_INSTALL_PATH} \
-        -U\*_URL -U\*_TAG \
-        -DUSE_SYSTEM_SWIG=On \
-        -DUSE_SYSTEM_Boost=On \
-        -DUSE_SYSTEM_Armadillo=On \
-        -DUSE_SYSTEM_FFTW3=On \
-        -DUSE_SYSTEM_HDF5=ON \
-        -DBUILD_siemens_to_ismrmrd=On \
-        -DUSE_ITK=ON \
-        -DDEVEL_BUILD=OFF\
-        -DNIFTYREG_USE_CUDA=OFF\
-        -DBUILD_CIL=ON\
-        -DPYTHON_EXECUTABLE="$PYTHON_EXECUTABLE"\
-        -DBUILD_pet_rd_tools=ON
-  cmake --build . -j${num_parallel}
 
-}
-
-# define a function to get the source
-# arguments: name_of_repo [git_ref]
-clone_or_pull()
-{
-  repoURL=$1
-  repo=`basename $1`
-  repo=${repo/.git//}
-  git_ref=${2:-master} # default to master
-  echo "======================  Getting/updating source for $repo"
-  cd $SIRF_SRC_PATH
-  if [ -d $repo ]
-  then
-    cd $repo
-    if [ $update_remote == 1 ]; then
-        git remote set-url origin $repoURL
-    fi
-    git fetch --tags --all
-  else
-    git clone --recursive $repoURL
-    cd $repo
-  fi
-  git checkout $git_ref
-  # check if we are not in detached HEAD state
-  if git symbolic-ref -q HEAD
-  then
-      # We are on a local branch.
-      echo "Warning: updating your local branch with 'git pull'"
-      git pull
-  fi
-  git submodule update --init
-}
-
-# define a function to build and install
-# arguments: name_of_repo [cmake arguments]
-build_and_install()
-{
-  repo=$1
-  shift
-  echo "======================  Building $repo"
-  cd $BUILD_PATH
-  if [ -d $repo ]
-  then
-    cd $repo
-    cmake .
-  else
-    mkdir $repo
-    cd $repo
-    cmake $* $SIRF_SRC_PATH/$repo
-  fi
-  echo "======================  Installing $repo"
-  make -j${num_parallel} install
-}
-
-# function to do everything
-update()
-{
-  clone_or_pull $1
-  build_and_install $*
-}
-
-# Launch the SuperBuild to update
-SuperBuild $SB_TAG
+# Launch the SuperBuild
+SuperBuild_install
 
 # copy scripts into the path
 cp -vp $SIRF_SRC_PATH/SIRF-SuperBuild/VirtualBox/scripts/update*sh $SIRF_INSTALL_PATH/bin
