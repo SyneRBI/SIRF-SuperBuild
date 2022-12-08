85f81c665a8059b92d48d3a28851f1ee4f13a596
Kris Thielemans <k.thielemans@ucl.ac.uk>
2022-12-08 22:15:17 +0000

    bump SIRF_TAG again to get rid of nose

--- version_config.cmake
+++ version_config.cmake
@@ -237,7 +237,7 @@ if (DEVEL_BUILD)
 else()
 
   #set(DEFAULT_SIRF_TAG v3.3.0)
-  set (DEFAULT_SIRF_TAG 95c748104578efcf73cb7894cf989cd7ae5879b5)
+  set (DEFAULT_SIRF_TAG 2ed708bb8c8dc4ac6e1b081a1f9800754621b5fd)
   
   ## STIR
   set(DEFAULT_STIR_URL https://github.com/UCL/STIR )
