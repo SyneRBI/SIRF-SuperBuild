5b29dd8875cd512d234ba93359136f102a265e87
Casper da Costa-Luis <casper.dcl@physics.org>
2023-11-27 15:27:16 +0000

    add docker-stacks submodule, cleanup build

--- jupyterhub/build_docker_stacks.sh
+++ jupyterhub/build_docker_stacks.sh
@@ -1,9 +1,10 @@
-# /bin/bash
+#!/usr/bin/env bash
+set -exuo pipefail
 
-set -ex
-pushd ../../docker-stacks/images/docker-stacks-foundation
+git submodule update --init --recursive
+pushd $(dirname "$0")/docker-stacks/images/docker-stacks-foundation
 # docker-stacks-foundations
-docker build --build-arg PYTHON_VERSION=3.9 --build-arg ROOT_CONTAINER=nvidia/cuda:11.7.1-cudnn8-runtime-ubuntu22.04 -t paskino/jupyter:docker-stacks-foundation-cuda11-cudnn8-runtime-ubuntu22.04 .
+docker build --build-arg PYTHON_VERSION=3.9 --build-arg BASE_CONTAINER=nvidia/cuda:11.7.1-cudnn8-runtime-ubuntu22.04 -t paskino/jupyter:docker-stacks-foundation-cuda11-cudnn8-runtime-ubuntu22.04 .
 
 # base notebook
 cd ../base-notebook
@@ -26,3 +27,4 @@ docker build --build-arg BASE_CONTAINER=paskino/jupyter:scipy-notebook-cuda11-cu
 
 popd
 
+docker build --build-arg BASE_IMAGE=paskino/jupyter:datascience-notebook-cuda11-cudnn8-runtime-ubuntu22.04 -t paskino/jupyter:sirf -f jupyterhub/Dockerfile .
