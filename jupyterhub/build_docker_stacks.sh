fbb69b9507dff080b2aebf62efc154a394bf354d
Casper da Costa-Luis <casper.dcl@physics.org>
2023-11-28 21:31:49 +0000

    safer directory switching

--- jupyterhub/build_docker_stacks.sh
+++ jupyterhub/build_docker_stacks.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -exuo pipefail
 
+pushd "$(dirname "${BASH_SOURCE[0]}")"
 git submodule update --init --recursive
-pushd $(dirname "$0")/docker-stacks/images/docker-stacks-foundation
 
+cd docker-stacks/images/docker-stacks-foundation
 docker build --build-arg PYTHON_VERSION=3.9 --build-arg BASE_CONTAINER=nvidia/cuda:11.7.1-cudnn8-runtime-ubuntu22.04 -t synerbi/jupyter:foundation .
 
 cd ../base-notebook
@@ -12,13 +13,13 @@ docker build --build-arg BASE_CONTAINER=synerbi/jupyter:foundation -t synerbi/ju
 cd ../minimal-notebook
 docker build --build-arg BASE_CONTAINER=synerbi/jupyter:base -t synerbi/jupyter:minimal .
 
-
 cd ../scipy-notebook
 docker build --build-arg BASE_CONTAINER=synerbi/jupyter:minimal -t synerbi/jupyter:scipy .
 
 cd ../datascience-notebook
 docker build --build-arg BASE_CONTAINER=synerbi/jupyter:scipy -t synerbi/jupyter:datascience .
 
-popd
-
+cd ../../../..
 docker build --build-arg BASE_CONTAINER=synerbi/jupyter:datascience -t synerbi/jupyter:sirf -f jupyterhub/Dockerfile .
+
+popd
