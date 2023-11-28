c66e745c3a360c81f8036fee413e18ab2e856562
Casper da Costa-Luis <casper.dcl@physics.org>
2023-11-28 17:11:09 +0000

    better image tags

--- jupyterhub/build_docker_stacks.sh
+++ jupyterhub/build_docker_stacks.sh
@@ -3,28 +3,22 @@ set -exuo pipefail
 
 git submodule update --init --recursive
 pushd $(dirname "$0")/docker-stacks/images/docker-stacks-foundation
-# docker-stacks-foundations
-docker build --build-arg PYTHON_VERSION=3.9 --build-arg BASE_CONTAINER=nvidia/cuda:11.7.1-cudnn8-runtime-ubuntu22.04 -t paskino/jupyter:docker-stacks-foundation-cuda11-cudnn8-runtime-ubuntu22.04 .
 
-# base notebook
+docker build --build-arg PYTHON_VERSION=3.9 --build-arg BASE_CONTAINER=nvidia/cuda:11.7.1-cudnn8-runtime-ubuntu22.04 -t synerbi/jupyter:foundation .
+
 cd ../base-notebook
-# change the base class with the BASE_CONTAINER argument
-# build and tag
-docker build --build-arg BASE_CONTAINER=paskino/jupyter:docker-stacks-foundation-cuda11-cudnn8-runtime-ubuntu22.04 -t paskino/jupyter:base-notebook-cuda11-cudnn8-runtime-ubuntu22.04 .
+docker build --build-arg BASE_CONTAINER=synerbi/jupyter:foundation -t synerbi/jupyter:base .
 
-# minimal notebook
 cd ../minimal-notebook
-docker build --build-arg BASE_CONTAINER=paskino/jupyter:base-notebook-cuda11-cudnn8-runtime-ubuntu22.04 -t paskino/jupyter:minimal-notebook-cuda11-cudnn8-runtime-ubuntu22.04 .
+docker build --build-arg BASE_CONTAINER=synerbi/jupyter:base -t synerbi/jupyter:minimal .
 
 
-# scipy-notebook
 cd ../scipy-notebook
-docker build --build-arg BASE_CONTAINER=paskino/jupyter:minimal-notebook-cuda11-cudnn8-runtime-ubuntu22.04 -t paskino/jupyter:scipy-notebook-cuda11-cudnn8-runtime-ubuntu22.04 .
+docker build --build-arg BASE_CONTAINER=synerbi/jupyter:minimal -t synerbi/jupyter:scipy .
 
-# datascience-notebook
 cd ../datascience-notebook
-docker build --build-arg BASE_CONTAINER=paskino/jupyter:scipy-notebook-cuda11-cudnn8-runtime-ubuntu22.04 -t paskino/jupyter:datascience-notebook-cuda11-cudnn8-runtime-ubuntu22.04 .
+docker build --build-arg BASE_CONTAINER=synerbi/jupyter:scipy -t synerbi/jupyter:datascience .
 
 popd
 
-docker build --build-arg BASE_IMAGE=paskino/jupyter:datascience-notebook-cuda11-cudnn8-runtime-ubuntu22.04 -t paskino/jupyter:sirf -f jupyterhub/Dockerfile .
+docker build --build-arg BASE_CONTAINER=synerbi/jupyter:datascience -t synerbi/jupyter:sirf -f jupyterhub/Dockerfile .
