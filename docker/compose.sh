560558b128fc9f701ce0d4d7ee43a8fca097fd7e
Casper da Costa-Luis <casper.dcl@physics.org>
2024-01-08 11:03:39 +0000

    fix GPU build

--- docker/compose.sh
+++ docker/compose.sh
@@ -35,7 +35,7 @@ echo "build args: $@"
 DCC_CPU="docker compose"
 DCC_GPU="docker compose -f docker-compose.yml -f docker/docker-compose.gpu.yml"
 
-pushd "$(dirname "${BASH_SOURCE[0]}")"
+pushd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 git submodule update --init --recursive
 
 echo build ccache
