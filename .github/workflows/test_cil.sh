1fdf1ab9f28aaf6b98aed73a2a4824f58ce51b08
Casper da Costa-Luis <casper.dcl@physics.org>
2024-03-01 13:49:16 +0000

    CIL: temporary test fix

--- .github/workflows/test_cil.sh
+++ .github/workflows/test_cil.sh
@@ -5,6 +5,7 @@ set -euxo pipefail
 mkdir cil_sirf_test
 touch cil_sirf_test/__init__.py
 DEFAULT_CIL_TAG=$(python -c 'import cil; print(f"v{cil.version.version}" if cil.version.commit_hash == "-1" else cil.version.commit_hash[1:])' | tail -n1)
+DEFAULT_CIL_TAG=master  # TODO: revert after CIL 24.0.0 release https://github.com/TomographicImaging/CIL/issues/1732
 for fname in test_SIRF.py utils.py testclass.py; do
   curl -fsSL https://raw.githubusercontent.com/TomographicImaging/CIL/$DEFAULT_CIL_TAG/Wrappers/Python/test/$fname > cil_sirf_test/$fname
 done
