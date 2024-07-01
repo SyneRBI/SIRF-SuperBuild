0329dc15b22c638ebf83a965e25f1f48609a5270
Kris Thielemans <KrisThielemans@users.noreply.github.com>
2024-07-01 22:40:24 +0100

    docker: use CMake 3.29.6

--- docker/build_essential-ubuntu.sh
+++ docker/build_essential-ubuntu.sh
@@ -11,7 +11,6 @@ apt-get install -yq --no-install-recommends \
   g++             \
   gcc             \
   man             \
-  cmake           \
   ninja-build     \
   ccache          \
   sudo            \
@@ -21,10 +20,10 @@ apt-get clean
 pushd $INSTALL_DIR
 
 # CMake
-#curl -o cmake.tgz -L https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1-linux-x86_64.tar.gz
-#tar xzf cmake.tgz && rm cmake.tgz
-#ln -s cmake-*x86_64 cmake || true
-#export PATH="$PWD/cmake/bin:$PATH"
+curl -o cmake.tgz -L https://github.com/Kitware/CMake/releases/download/v3.29.6/cmake-3.29.6-linux-x86_64.tar.gz
+tar xzf cmake.tgz && rm cmake.tgz
+ln -s cmake-*x86_64 cmake || true
+export PATH="$PWD/cmake/bin:$PATH"
 
 # ccache
 mkdir -p bin
