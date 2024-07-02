d6fc94a0236e91de0b262cab534716db235cd6b0
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2024-07-02 16:36:34 +0000

    bump CIL version

--- version_config.cmake
+++ version_config.cmake
@@ -161,7 +161,8 @@ set(DEFAULT_JSON_TAG v3.11.3)
 # CCPi CIL
 # minimum supported version of CIL supported is > 22.1.0 or from commit a6062410028c9872c5b355be40b96ed1497fed2a
 set(DEFAULT_CIL_URL https://github.com/TomographicImaging/CIL)
-set(DEFAULT_CIL_TAG ccf17f393ba911d13b74f2327779dde030098fe6) # 28 May 2024
+# set(DEFAULT_CIL_TAG ccf17f393ba911d13b74f2327779dde030098fe6) # 28 May 2024
+set(DEFAULT_CIL_TAG b3d8ffeeec9413a3b54264e4097a445d2ef5b88b) # 28 May 2024
 
 set(DEFAULT_CCPi-Regularisation-Toolkit_URL https://github.com/TomographicImaging/CCPi-Regularisation-Toolkit)
 set(DEFAULT_CCPi-Regularisation-Toolkit_TAG "v24.0.1")
