0c8b7134cefb54676ef21ffc1cb85e4904b9487f
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2024-08-23 15:31:14 +0100

    bump CIL version to one with SAG/SAGA SVRG/LSVRG

--- version_config.cmake
+++ version_config.cmake
@@ -161,7 +161,7 @@ set(DEFAULT_JSON_TAG v3.11.3)
 # CCPi CIL
 # minimum supported version of CIL supported is > 22.1.0 or from commit a6062410028c9872c5b355be40b96ed1497fed2a
 set(DEFAULT_CIL_URL https://github.com/TomographicImaging/CIL)
-set(DEFAULT_CIL_TAG v24.1.0)
+set(DEFAULT_CIL_TAG 4f6e3cfb0648d08071974eba35c1369a892d4924) # 23/08/24
 
 set(DEFAULT_CCPi-Regularisation-Toolkit_URL https://github.com/TomographicImaging/CCPi-Regularisation-Toolkit)
 set(DEFAULT_CCPi-Regularisation-Toolkit_TAG "v24.0.1")
